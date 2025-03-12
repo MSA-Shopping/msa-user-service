159188517421fcdfc65b124d3ecb395c26e5ef29
WonGi-Kim <kwg970310@gmail.com>
2025-03-12 18:24:55 +0900

    feat : User 관련 파일 생성 및 회원가입 서비스 로직 구현

---
+++ src/main/java/com/example/msauserservice/domain/users/UserController.java
@@ -0,0 +1,33 @@
+package com.example.msauserservice.domain.users;
+
+import com.example.msauserservice.domain.users.dto.SignupRequestDto;
+import com.example.msauserservice.global.common.CommonResponse;
+import com.example.msauserservice.global.exception.CustomException;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1")
+public class UserController {
+    private final UserService userService;
+
+    // 회원가입
+    @PostMapping("/users/signup")
+    public ResponseEntity<CommonResponse> signup(@RequestBody @Valid SignupRequestDto requestDto) {
+        try {
+            userService.signup(requestDto);
+            CommonResponse response = new CommonResponse("회원가입 성공", 201, "");
+            return ResponseEntity.ok(response);
+        } catch (CustomException e) {
+            CommonResponse response = new CommonResponse("회원가입 실패", e.getStatusCode().value(), e.getMessage());
+            return ResponseEntity.badRequest().body(response);
+        }
+    }
+
+}
---
+++ src/main/java/com/example/msauserservice/domain/users/UserService.java
@@ -0,0 +1,32 @@
+package com.example.msauserservice.domain.users;
+
+import com.example.msauserservice.domain.users.dto.SignupRequestDto;
+import com.example.msauserservice.global.exception.CustomException;
+import com.example.msauserservice.global.exception.ErrorCode;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void signup(SignupRequestDto requestDto) {
+        // 저장하기 전 User 객체 생성
+        User newUser = User.builder()
+                .username(requestDto.getUsername())
+                .email(requestDto.getEmail())
+                .password(requestDto.getPassword())
+                .phoneNumber(requestDto.getPhoneNumber())
+                .build();
+
+        // User가 이미 존재하는지 확인
+        if(userRepository.findByEmail(newUser.getEmail()) != null) {
+            throw new CustomException(ErrorCode.CONFLICT_USER);
+        } else {
+            userRepository.save(newUser);
+        }
+    }
+}
