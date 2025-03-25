81b90ba7bdad0fc3d47bcb7b615a7146d2477f64
WonGi-Kim <kwg970310@gmail.com>
2025-03-25 18:05:48 +0900

    feat : 모든 요청을 허용하여 SpringSecurity 가 아닌 Interceptor 에서 검증 과정이 수행되도록 구현

--- src/main/java/com/example/msauserservice/global/config/SecurityConfig.java
+++ src/main/java/com/example/msauserservice/global/config/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.example.msauserservice.global.config;
 
+import com.example.msauserservice.global.interceptor.GatewayAuthenticationInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -13,12 +15,17 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+@RequiredArgsConstructor
+public class SecurityConfig implements WebMvcConfigurer {
+
+    private final GatewayAuthenticationInterceptor gatewayAuthenticationInterceptor;
 
     @Bean
     public PasswordEncoder getPasswordEncoder() {
@@ -26,20 +33,19 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정 추가
-                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-                        .requestMatchers("/v1/**").permitAll()
-
-                        .anyRequest().authenticated()
+                        .requestMatchers("/v1/users/email","/v1/users/signup").permitAll()
+                        .requestMatchers("/v1/users/**").permitAll()  // 모든 /v1/users/** 경로 허용
+                        .anyRequest().permitAll()  // 다른 모든 요청도 허용
                 );
-
         return http.build();
     }
 
@@ -54,5 +60,17 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(gatewayAuthenticationInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                    "/error",
+                    "/v1/users/signup",
+                    "/v1/users/email",
+                    "/v1/auth/login"
+                );
     }
 }
