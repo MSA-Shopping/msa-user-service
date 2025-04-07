3529d8980c2dbe7a29dfaa38dd6cb1ec22353794
WonGi-Kim <kwg970310@gmail.com>
2025-04-07 17:43:00 +0900

    fix : Controller 에서 Header 검증

--- src/main/java/com/example/msauserservice/domain/users/UserController.java
+++ src/main/java/com/example/msauserservice/domain/users/UserController.java
@@ -5,6 +5,8 @@ import com.example.msauserservice.domain.users.dto.SignupRequestDto;
 import com.example.msauserservice.domain.users.dto.UserDto;
 import com.example.msauserservice.global.common.CommonResponse;
 import com.example.msauserservice.global.exception.CustomException;
+import com.example.msauserservice.global.exception.ErrorCode;
+import com.example.msauserservice.global.security.GatewaySignatureVerifier;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +18,7 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/v1")
 public class UserController {
     private final UserService userService;
+    private final GatewaySignatureVerifier gatewaySignatureVerifier;
 
     // 회원가입
     @PostMapping("/users/signup")
@@ -42,12 +45,22 @@ public class UserController {
 
     // gateway를 통해 발급된 인증 객체로 인증해야 함
     @GetMapping("/users/needtoken")
-    public Mono<ResponseEntity<CommonResponse>> needToken(@RequestHeader("X-User-Id") String userId) {
-        if (userId == null) {
-            return Mono.just(ResponseEntity.status(401).body(new CommonResponse("인증 실패", 401, "")));
+    public ResponseEntity<CommonResponse> needToken(@RequestHeader("X-User-Id") String userId,
+                                                          @RequestHeader("X-Timestamp") String timestamp,
+                                                          @RequestHeader("X-Signature") String signature) {
+
+        // 헤더 검증
+        try{
+            if(userId == null || timestamp == null || signature == null ||
+                       !gatewaySignatureVerifier.isValidSignature(userId, Long.parseLong(timestamp), signature)){
+                        throw new CustomException(ErrorCode.HEADER_NOT_FOUND);
+            }
+            CommonResponse response = new CommonResponse("토큰이 필요한 페이지", 200, userId);
+            return ResponseEntity.ok(response);
+        } catch (CustomException e) {
+            CommonResponse response = new CommonResponse("인증 실패", e.getStatusCode().value(), e.getMessage());
+            return ResponseEntity.status(e.getStatusCode()).body(response);
         }
-        CommonResponse response = new CommonResponse("토큰이 필요한 페이지", 200, userId);
-        return Mono.just(ResponseEntity.ok(response));
     }
 
     // 삭제
